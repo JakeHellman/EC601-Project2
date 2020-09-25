068e19a8dd74173bdc5b573b6950fb90b33ec20d
Jake Hellman <jake@Jakes-MacBook-Pro.local>
2020-09-24 23:29:05 -0400

    adding hello world demo for google nlp

---
+++ phase1a.py
@@ -0,0 +1,5 @@
+# Phase 1(a): Twitter APIs
+# Write test programs to exercise different twitter APIs.
+# For example, retrieving tweets, searching per time, hashtags, etc.
+# All your programs should be on GitHub including a README file explaining your tests and results.
+
---
+++ phase1b.py
@@ -0,0 +1,23 @@
+# Phase1(b): Google NLP
+# Write test programs to exercise different Google BLP APIs.  Focus on Sentiment analysis.
+# All your programs should be on GitHub including a README file explaining your tests and results.
+
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+# Instantiates a client
+client = language.LanguageServiceClient()
+
+# The text to analyze
+text = u'Hello, world!'
+document = types.Document(
+    content=text,
+    type=enums.Document.Type.PLAIN_TEXT)
+
+# Detects the sentiment of the text
+sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+print('Text: {}'.format(text))
+print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
