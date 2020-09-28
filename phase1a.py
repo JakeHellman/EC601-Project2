e4bc30c5e0df2327ebdd4f21cd6e07c755e0e989
Jake Hellman <jake@Jakes-MacBook-Pro.local>
2020-09-28 00:01:15 -0400

    writing phase1a program to test Twitter API and adding readme

--- phase1a.py
+++ phase1a.py
@@ -3,3 +3,18 @@
 # For example, retrieving tweets, searching per time, hashtags, etc.
 # All your programs should be on GitHub including a README file explaining your tests and results.
 
+import twitter_keys
+
+import tweepy
+import csv
+
+auth = tweepy.OAuthHandler(twitter_keys.API_key, twitter_keys.API_secret_key)
+auth.set_access_token(twitter_keys.Access_token, twitter_keys.Access_token_secret)
+api = tweepy.API(auth)
+
+csvFile = open('mytweets.csv', 'w')
+csvWriter = csv.writer(csvFile)
+
+for tweet in tweepy.Cursor(api.search,q="#Celtics",count=100, lang="en", since="2020-09-28").items():
+	print(tweet.created_at, tweet.text)
+	csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
---
+++ readme.txt
@@ -0,0 +1,7 @@
+This repository is for EC 601 Project #2.
+
+phase1a.py
+This is a python program which uses the Twitter API in a basic way. It requests tweets with a specific hashtag (in this case, "#Celtics"). Note that if this program is re-run, the date may need to be adjusted. The matching tweets are printed to the terminal and also written to a csv file.
+
+phase1b.py
+This is a python program which interacts with the Google Natural Language Processing API in a basic way. It sends two sentences to Google's NLP and prints out the sentiment of each of the sentences individually, as well as the average for the two sentences.
